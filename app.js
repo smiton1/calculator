386510de51e928e5a9191381c8f013b816289e57
smiton1 <182565156+smiton1@users.noreply.github.com>
2024-10-07 07:01:32 -0500

    connected buttons to log number values, need to map to calculator obj

--- app.js
+++ app.js
@@ -1,38 +1,10 @@
-const button = document.querySelectorAll("button")
+const buttons = document.querySelectorAll(".button")
+buttons.forEach(button=>{
+    button.addEventListener("click",()=>{
+        console.log(Number(button.innerHTML))
+    })
+})
 
-function add(a, b){
-    return a+b
-}
-
-function subtract(a, b){
-    return a-b
-}
-
-function multiply(a, b){
-    return a * b
-}
-
-function divide(a, b){
-    return a/b
-}
-
-function operate(a,b,c){
-    let total = 0
-    if(c==add){
-        total = add(a,b)
-        return total
-    }else if(c==subtract){
-        total = subtract(a,b)
-        return total
-    }else if(c == multiply){
-        total = multiply(a,b)
-        return total
-    }else if(c== divide){
-        total = divide(a,b)
-        return total
-    }
-    
-}
 
 const calculator = {
     clear: "c",
@@ -56,7 +28,68 @@ const calculator = {
     },
     divide: function(a,b){
         return this.number[a] / this.number[b]
+    },
+    operate: function(a,b,c){
+        let total= 0
+        a = prompt('first number')
+        b = prompt('second number')
+        c = prompt('operator')
+        if(c=="+"){
+            total = add(a,b)
+            return total
+        }else if(c=="-"){
+            total = subtract(a,b)
+            return total
+        }else if(c == "*"){
+            total = multiply(a,b)
+            return total
+        }else if(c== "/"){
+            total = divide(a,b)
+            return total
+        }
     }
+}
+let numOne = 0
+let numTwo = 0
 
+
+function operate(){
 
 }
+
+
+
+
+// function add(a, b){
+//     return a+b
+// }
+
+// function subtract(a, b){
+//     return a-b
+// }
+
+// function multiply(a, b){
+//     return a * b
+// }
+
+// function divide(a, b){
+//     return a/b
+// }
+
+// function operate(a,b,c){
+//     let total = 0
+//     if(c==add){
+//         total = add(a,b)
+//         return total
+//     }else if(c==subtract){
+//         total = subtract(a,b)
+//         return total
+//     }else if(c == multiply){
+//         total = multiply(a,b)
+//         return total
+//     }else if(c== divide){
+//         total = divide(a,b)
+//         return total
+//     }
+    
+// }
